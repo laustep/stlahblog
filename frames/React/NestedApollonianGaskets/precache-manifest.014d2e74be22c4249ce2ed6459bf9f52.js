0ae24dd9b090a3ea72d4aa10a85f1ba856d9494c
stla <laurent_step@yahoo.fr>
2020-01-16 20:29:43 +0100

    React nested Apollonian

---
+++ frames/React/NestedApollonianGaskets/precache-manifest.014d2e74be22c4249ce2ed6459bf9f52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b64d2b65fa67178269b768a589e577c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ebf2c0b76ec824da772",
+    "url": "/static/css/main.77ea3736.chunk.css"
+  },
+  {
+    "revision": "408c34acb34628773a90",
+    "url": "/static/js/2.54f5b2fa.chunk.js"
+  },
+  {
+    "revision": "4a1b2ae6587c608a12a524d1cf46a7e8",
+    "url": "/static/js/2.54f5b2fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ebf2c0b76ec824da772",
+    "url": "/static/js/main.29759bd9.chunk.js"
+  },
+  {
+    "revision": "2c61b5015c3942d854e3",
+    "url": "/static/js/runtime-main.1685a7b5.js"
+  }
+]);
